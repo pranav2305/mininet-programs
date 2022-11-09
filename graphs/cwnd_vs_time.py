ec0777752e7c478891e46b6a5505ecb4a5633f22
pranav2305 <pranav2305@gmail.com>
2022-11-10 01:54:41 +0530

    Add graphs

--- graphs/cwnd_vs_time.py
+++ graphs/cwnd_vs_time.py
@@ -1,8 +1,12 @@
 import json
 import matplotlib.pyplot as plt
-from timings import *
+import argparse
 
-json_object = open('out.json')
+parser = argparse.ArgumentParser(description='Plot cwnd vs time')
+parser.add_argument('--file', type=str, help='The file to plot')
+args = parser.parse_args()
+
+json_object = open(args.file)
 data = json.load(json_object)
 
 i=1
@@ -11,7 +15,7 @@ x = []
 for entry in data['intervals']:
     
     
-    datapoint = entry['streams'][0]
+    datapoint = entry['streams'][i]
     time  = i
     bts = datapoint["snd_cwnd"]
     y.append(bts)
--- graphs/throughput_vs_time.py
+++ graphs/throughput_vs_time.py
@@ -1,8 +1,12 @@
 import json
 import matplotlib.pyplot as plt
-from timings import *
+import argparse
 
-json_object = open('out.json')
+parser = argparse.ArgumentParser(description='Plot cwnd vs time')
+parser.add_argument('--file', type=str, help='The file to plot')
+args = parser.parse_args()
+
+json_object = open(args.file)
 data = json.load(json_object)
 
 i=1
