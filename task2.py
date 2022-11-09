43fcc1cf3506aaf1935f74104a6c1170e482ad39
pranav2305 <pranav2305@gmail.com>
2022-11-09 10:30:40 +0530

    Add Task 2

--- task2.py
+++ task2.py
@@ -16,14 +16,12 @@ def startNetwork():
 
     s1 = net.addSwitch("s1")
     s2 = net.addSwitch("s2")
-    s3 = net.addSwitch("s3")
 
     net.addLink(s1, h1)
-    net.addLink(s1, h2)
-    net.addLink(s2, h3)
+    net.addLink(s1, h3)
+    net.addLink(s2, h2)
     net.addLink(s2, h4)
-    net.addLink(s3, s1)
-    net.addLink(s3, s2)
+    net.addLink(s1, s2)
 
     net.build()
     c0.start()
